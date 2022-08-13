66f94a3f234839c5f6e4d1b971ce66ad41c9722f
rishikeshan007 <65391753+rishikeshan007@users.noreply.github.com>
2022-08-14 00:05:47 +0530

    Create sumfrom1toN.cpp

---
+++ Basic programs/sumfrom1toN.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    
+    cout<<"Enter N"<<endl;
+    cin>>n;
+    int res = ((1+n)/2)*n;
+    cout<<"sum of 1 to N is:"<<res<<endl;
+    
+}
