25b75fbda96881d5a9b307173de8f85050a8f138
rishikeshan007 <65391753+rishikeshan007@users.noreply.github.com>
2022-08-14 19:14:24 +0530

    Create pattern6.cpp

---
+++ Basic programs/pattern6.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    int n;
+    cin>>n;
+    int count;
+    for(int i=1;i<=n;i++)
+    {
+        
+        for(int j=1;j<=n;j++)
+        {
+            char c = 'A'+i-1;
+            cout<<c<<" ";
+            
+        }
+        cout<<endl;
+    }
+}
