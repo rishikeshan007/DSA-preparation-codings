fe12ccb81e021485423e6ecbef62a91788eab85b
rishikeshan007 <65391753+rishikeshan007@users.noreply.github.com>
2022-08-14 19:31:51 +0530

    Create pattern8.cpp

---
+++ Basic programs/pattern8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    int n;
+    cin>>n;
+    char start='A';
+    for(int i=0;i<n;i++)
+    {
+       
+        for(int j=0;j<n;j++)
+        {
+            cout<<start<<" ";
+            start++;
+            
+            
+        }
+        cout<<endl;
+    }
+}
